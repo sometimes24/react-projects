b0439c324c49e933225835a63bca9bf13a420129
einasmadi <einasmadi@gmail.com>
2020-05-17 19:14:44 -0400

    Remove console statement

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -5,14 +5,8 @@ import { Provider } from "react-redux";
 import { AppRouter } from "./router/AppRouter";
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
-import { addExpense } from "./redux/actions/expenses";
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: "Water Bill", amount: 2000 }));
-store.dispatch(addExpense({ description: "Gas Bill", amount: 7600, createdAt: 200 }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
